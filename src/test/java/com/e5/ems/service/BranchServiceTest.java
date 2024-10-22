2e507246679d1bac8cf2005f51ce81ad6c503967
Saravana-Pon-Balan <saravanafrancis16@gmail.com>
2024-10-22 10:49:10 +0530

    Revert "check mixed done"
    This reverts commit 6b4dee36891060f976ac1338125a4ac0bda3f42d.

--- src/test/java/com/e5/ems/service/BranchServiceTest.java
+++ src/test/java/com/e5/ems/service/BranchServiceTest.java
@@ -7,6 +7,7 @@ package com.e5.ems.service;
 //checking cherry pick
 // comment changed in branch
 // amend change
+// amend changed
 import java.util.Date;
 import java.util.NoSuchElementException;
 
