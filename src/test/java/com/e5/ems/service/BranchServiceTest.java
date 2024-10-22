81ddbec8bce41a9cac1ae2a69c9035b472e31b32
Saravana-Pon-Balan <saravanafrancis16@gmail.com>
2024-10-22 10:45:03 +0530

    Revert "Amend checked success"
    This reverts commit 50918cc5621f56d06fb4c7d73eee0ef78c88497e.

--- src/test/java/com/e5/ems/service/BranchServiceTest.java
+++ src/test/java/com/e5/ems/service/BranchServiceTest.java
@@ -6,7 +6,7 @@ package com.e5.ems.service;
 //checking cherry-pick
 //checking cherry pick
 // comment changed in branch
-// amend changed
+// amend change
 import java.util.Date;
 import java.util.NoSuchElementException;
 
